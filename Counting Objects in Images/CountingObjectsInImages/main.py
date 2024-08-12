ab767ed2f6e4c0a9caadf0ccfa0d5f6fd62abb6a
David Andrés Valencia García <david.andres.2801@gmail.com>
2024-08-12 09:20:07 -0500

    :construction: ADD: Definition of tasks to be performed for the project

--- Counting Objects in Images/CountingObjectsInImages/main.py
+++ Counting Objects in Images/CountingObjectsInImages/main.py
@@ -1,2 +1,27 @@
-if __name__ == '__main__':
+# Importe de librerias necesarias
+import cv2
+
+
+# Definicion de la funcion principal
+def main():
     print('Hello, World!')
+
+
+# Obtener la imagen
+img = cv2.imread('assets/monedas_colombia.jpg', 1)
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# TODO: Obtener la imagen
+# TODO: Procesarla para obtener las monedas
+# TODO: Contar las monedas
+# TODO: Mostrar la imagen con las monedas marcadas
+# TODO: Mostrar el número de monedas
+# TODO: Guardar la imagen con las monedas marcadas
+# TODO: Crear un archivo de texto con el número de monedas
+# TODO: Generar un pdf con el algoritmo implementado, los pasos y los resultados obtenidos
+
+# Código para poder ejecutar el archivo
+if __name__ == '__main__':
+    main()
